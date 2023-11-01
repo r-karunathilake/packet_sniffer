95bbed323c4d07f3eb8cc50cca1bb25eac3c044a
Ravindu Karunathilake <karunath@ualberta.ca>
2023-11-01 17:33:43 -0600

    Added traffic filtering functionality based on user provided arguments.

--- packet_sniffer.c
+++ packet_sniffer.c
@@ -17,8 +17,8 @@
 #include <pcap.h>         // Access copies of packets off the wire.
 #include <stdlib.h>       // For exit()
 #include <netinet/in.h>   // 'in_addr' structure declaration used by 'inet_ntoa()'
-#include <arpa/inet.h>    // Provides 'inet_ntoa()' declaration 
-#include <net/ethernet.h> // Provides ethernet header declaration 
+#include <arpa/inet.h>    // Provides 'inet_ntoa()' declaration
+#include <net/ethernet.h> // Provides ethernet header declaration
 
 FILE *logFile = NULL;
 
@@ -33,10 +33,17 @@ int main(int argc, char *argv[])
   char *pDeviceName = NULL;           // The network interface name to be sniffed
   char errorBuffer[PCAP_ERRBUF_SIZE]; // Error string of size 256
   char *ip_addr = NULL;               // Dot notation IP address
-  char *mask_addr = NULL;             // Dot notation mask address 
+  char *mask_addr = NULL;             // Dot notation mask address
   bpf_u_int32 mask;                   // Interface network mask
   bpf_u_int32 ip;                     // Interface IP
-  struct in_addr addr;                // IPv4 address conversion structure 
+  struct bpf_program filter;          // Compiled filter structure 
+  u_char *callbackArgs;               // Arguments for 'process_packet()'
+ 
+  if (argc < 2)
+  {
+    fprintf(stderr, "Usage: %s <maximum_packets_to_capture> <optional: filter>\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
 
   // Get a list of all available devices
   printf("Finding available devices ... ");
@@ -62,33 +69,38 @@ int main(int argc, char *argv[])
 
   printf("\nSelecting default device ... \n");
   // Get the device IP and mask in byte order
-  if(pcap_lookupnet(pDeviceName, &ip, &mask, errorBuffer) == -1){ 
-    fprintf(stderr, "Couldn't get netmask for device %s: %s\n", pDeviceName, 
+  if (pcap_lookupnet(pDeviceName, &ip, &mask, errorBuffer) == -1)
+  {
+    fprintf(stderr, "Couldn't get netmask for device %s: %s\n", pDeviceName,
             errorBuffer);
     ip = 0;
     mask = 0;
   }
-  // Convert the IP and mask from bytes to IPv4 dotted-decimal notation 
+  printf("  Device: %s\n", pDeviceName);
+
+  // Convert the IP and mask from bytes to IPv4 dotted-decimal notation
+  struct in_addr addr; // IPv4 address conversion structure
   addr.s_addr = ip;
   ip_addr = inet_ntoa(addr);
-  if(ip_addr == NULL){
+  if (ip_addr == NULL)
+  {
     perror("inet_ntoa()");
     exit(EXIT_FAILURE);
   }
+  printf("  IP: %s\n", ip_addr);
 
   addr.s_addr = mask;
   mask_addr = inet_ntoa(addr);
-  if(mask_addr == NULL){
+  if (mask_addr == NULL)
+  {
     perror("inet_ntoa()");
     exit(EXIT_FAILURE);
   }
-  printf("  Device: %s\n", pDeviceName);
-  printf("  IP: %s\n", ip_addr);
   printf("  Mask: %s\n", mask_addr);
 
   // Sniffing in promiscuous mode
   printf("\nOpening device '%s' for sniffing ... ", pDeviceName);
-  pHandle = pcap_open_live(pDeviceName, BUFSIZ, 1, 1000, errorBuffer);
+  pHandle = pcap_open_live(pDeviceName, BUFSIZ, 1, 10000, errorBuffer);
   if (pHandle == NULL)
   {
     fprintf(stderr, "Couldn't open device '%s': %s\n", pDeviceName, errorBuffer);
@@ -103,9 +115,24 @@ int main(int argc, char *argv[])
     printf("WARNING: Unsuccessful in creating log file!");
   }
 
+  if(argc > 2){
+    // Compile and apply packet filter
+    if (pcap_compile(pHandle, &filter, argv[2], 0, mask) == -1)
+    {
+      fprintf(stderr, "Error compiling packet filter: %s\n", argv[2]);
+      exit(EXIT_FAILURE);
+    }
+    if (pcap_setfilter(pHandle, &filter) == -1)
+    {
+      fprintf(stderr, "Unable to set requested packet filter: %s\n", argv[2]);
+      exit(EXIT_FAILURE);
+    }
+  }
+
   /* Put the selected device in a packet capture loop with callback function
   'process_packet()' */
-  pcap_loop(pHandle, -1, process_packet, NULL);
+  pcap_loop(pHandle, atoi(argv[1]), process_packet, callbackArgs);
+  fprintf(stdout, "Finished processing packets. Running clean-up ...\n");
 
   // Free all the devices from memory
   printf("Freeing all network devices and closing session ... ");
@@ -118,5 +145,9 @@ int main(int argc, char *argv[])
 void process_packet(u_char *args, const struct pcap_pkthdr *header,
                     const u_char *packet)
 {
-  printf("Inside 'process_packet()'");
+  static int count = 1;
+  fprintf(stdout, "Packet count: %d\n", count);
+
+  fflush(stdout);
+  count++;
 }
