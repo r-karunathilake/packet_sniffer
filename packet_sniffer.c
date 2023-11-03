cb0a8ceeac9a2884f8f6f62c021effdd5564a532
Ravindu Karunathilake <karunath@ualberta.ca>
2023-11-02 20:53:31 -0600

    Added ethernet header information to log file.

--- packet_sniffer.c
+++ packet_sniffer.c
@@ -3,200 +3,246 @@
   library for low-level network access. The program is written and
   tested in Ubuntu.
 
-    Ubuntu version:         22.04.3 LTS
-    lippcap version:        1.10.1
-    C/C++ compiler version: GCC 11.4.0
-    Linux kernel version:   5.15.90.1-microsoft-standard-WSL2
+	Ubuntu version:         22.04.3 LTS
+	lippcap version:        1.10.1
+	C/C++ compiler version: GCC 11.4.0
+	Linux kernel version:   5.15.90.1-microsoft-standard-WSL2
 
   Author: Ravindu Karunathilake
   Date: 2023/10/31
 */
 
 #include <stdio.h>
-#include <string.h>       // For strcpy()
-#include <pcap.h>         // Access copies of packets off the wire.
-#include <stdlib.h>       // For exit()
-#include <netinet/in.h>   // 'in_addr' structure declaration used by 'inet_ntoa()'
-#include <arpa/inet.h>    // Provides 'inet_ntoa()' declaration
-#include <net/ethernet.h> // Provides ethernet header declaration
-#include <netinet/ether.h>// Provides 'ether_nota()' and 'ether_addr()'
+#include <string.h>		   // For strcpy()
+#include <pcap.h>		   // Access copies of packets off the wire.
+#include <stdlib.h>		   // For exit()
+#include <netinet/in.h>	   // 'in_addr' structure declaration used by 'inet_ntoa()'
+#include <netinet/ether.h> // Provides 'ether_nota()' and 'ether_addr()'
+#include <arpa/inet.h>	   // Provides 'inet_ntoa()' declaration
+#include <net/ethernet.h>  // Provides ethernet header declaration
+#include <netinet/ip.h>	   // Provides IP header declaration
 
 FILE *logFile = NULL;
+// Count the number of IP packets
+static int total = 0;
+static int icmp_count = 0;
+static int igmp_count = 0;
+static int tcp_count = 0;
+static int udp_count = 0;
+static int other_count = 0;
 
 // Function prototypes
 void process_packet(u_char *, const struct pcap_pkthdr *, const u_char *);
+void parse_ip_packet(u_char *, const struct pcap_pkthdr *, const u_char *);
 u_int16_t parse_ethernet_header(u_char *, const struct pcap_pkthdr *, const u_char *);
 
 int main(int argc, char *argv[])
 {
-  pcap_t *pHandle;                    // Session handle
-  pcap_if_t *pAllDevices = NULL;      // List of network interfaces
-  pcap_if_t *pDevice = NULL;          // A network interface
-  char *pDeviceName = NULL;           // The network interface name to be sniffed
-  char errorBuffer[PCAP_ERRBUF_SIZE]; // Error string of size 256
-  char *ip_addr = NULL;               // Dot notation IP address
-  char *mask_addr = NULL;             // Dot notation mask address
-  bpf_u_int32 mask;                   // Interface network mask
-  bpf_u_int32 ip;                     // Interface IP
-  struct bpf_program filter;          // Compiled filter structure 
-  u_char *callbackArgs;               // Arguments for 'process_packet()'
- 
-  if (argc < 2)
-  {
-    fprintf(stderr, "Usage: %s <maximum_packets_to_capture> <optional: filter>\n", argv[0]);
-    exit(EXIT_FAILURE);
-  }
+	pcap_t *pHandle;					// Session handle
+	pcap_if_t *pAllDevices = NULL;		// List of network interfaces
+	pcap_if_t *pDevice = NULL;			// A network interface
+	char *pDeviceName = NULL;			// The network interface name to be sniffed
+	char errorBuffer[PCAP_ERRBUF_SIZE]; // Error string of size 256
+	char *ip_addr = NULL;				// Dot notation IP address
+	char *mask_addr = NULL;				// Dot notation mask address
+	bpf_u_int32 mask;					// Interface network mask
+	bpf_u_int32 ip;						// Interface IP
+	struct bpf_program filter;			// Compiled filter structure
+	u_char *callbackArgs;				// Arguments for 'process_packet()'
 
-  // Get a list of all available devices
-  printf("Finding available devices ... ");
-  if (pcap_findalldevs(&pAllDevices, errorBuffer))
-  {
-    fprintf(stderr, "Encountered an error in finding devices : %s", errorBuffer);
-    exit(EXIT_FAILURE);
-  }
-  printf("Done\n");
+	if (argc < 2)
+	{
+		fprintf(stderr, "Usage: %s <maximum_packets_to_capture> <optional: filter>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
 
-  printf("Found following devices:\n");
-  int deviceNum = 1;
-  for (pDevice = pAllDevices; pDevice != NULL; pDevice = pDevice->next)
-  {
-    // Select the default interface (first in the list)
-    if (deviceNum == 1)
-    {
-      pDeviceName = pDevice->name;
-    }
-    printf("  %d. %s - %s\n", deviceNum, pDevice->name, pDevice->description);
-    deviceNum++;
-  }
+	// Get a list of all available devices
+	printf("Finding available devices ... ");
+	if (pcap_findalldevs(&pAllDevices, errorBuffer))
+	{
+		fprintf(stderr, "Encountered an error in finding devices : %s", errorBuffer);
+		exit(EXIT_FAILURE);
+	}
+	printf("Done\n");
 
-  printf("\nSelecting default device ... \n");
-  // Get the device IP and mask in byte order
-  if (pcap_lookupnet(pDeviceName, &ip, &mask, errorBuffer) == -1)
-  {
-    fprintf(stderr, "Couldn't get netmask for device %s: %s\n", pDeviceName,
-            errorBuffer);
-    ip = 0;
-    mask = 0;
-  }
-  printf("  Device: %s\n", pDeviceName);
+	printf("Found following devices:\n");
+	int deviceNum = 1;
+	for (pDevice = pAllDevices; pDevice != NULL; pDevice = pDevice->next)
+	{
+		// Select the default interface (first in the list)
+		if (deviceNum == 1)
+		{
+			pDeviceName = pDevice->name;
+		}
+		printf("  %d. %s - %s\n", deviceNum, pDevice->name, pDevice->description);
+		deviceNum++;
+	}
 
-  // Convert the IP and mask from bytes to IPv4 dotted-decimal notation
-  struct in_addr addr; // IPv4 address conversion structure
-  addr.s_addr = ip;
-  ip_addr = inet_ntoa(addr);
-  if (ip_addr == NULL)
-  {
-    perror("inet_ntoa()");
-    exit(EXIT_FAILURE);
-  }
-  printf("  IP: %s\n", ip_addr);
+	printf("\nSelecting default device ... \n");
+	// Get the device IP and mask in byte order
+	if (pcap_lookupnet(pDeviceName, &ip, &mask, errorBuffer) == -1)
+	{
+		fprintf(stderr, "Couldn't get netmask for device %s: %s\n", pDeviceName,
+				errorBuffer);
+		ip = 0;
+		mask = 0;
+	}
+	printf("  Device: %s\n", pDeviceName);
 
-  addr.s_addr = mask;
-  mask_addr = inet_ntoa(addr);
-  if (mask_addr == NULL)
-  {
-    perror("inet_ntoa()");
-    exit(EXIT_FAILURE);
-  }
-  printf("  Mask: %s\n", mask_addr);
+	// Convert the IP and mask from bytes to IPv4 dotted-decimal notation
+	struct in_addr addr; // IPv4 address conversion structure
+	addr.s_addr = ip;
+	ip_addr = inet_ntoa(addr);
+	if (ip_addr == NULL)
+	{
+		perror("inet_ntoa()");
+		exit(EXIT_FAILURE);
+	}
+	printf("  IP: %s\n", ip_addr);
 
-  // Sniffing in promiscuous mode
-  printf("\nOpening device '%s' for sniffing ... ", pDeviceName);
-  pHandle = pcap_open_live(pDeviceName, BUFSIZ, 1, 10000, errorBuffer);
-  if (pHandle == NULL)
-  {
-    fprintf(stderr, "Couldn't open device '%s': %s\n", pDeviceName, errorBuffer);
-    exit(EXIT_FAILURE);
-  }
-  printf("Done\n");
+	addr.s_addr = mask;
+	mask_addr = inet_ntoa(addr);
+	if (mask_addr == NULL)
+	{
+		perror("inet_ntoa()");
+		exit(EXIT_FAILURE);
+	}
+	printf("  Mask: %s\n", mask_addr);
 
-  // Open a log file handler for traffic parsing output
-  logFile = fopen("log.txt", "w");
-  if (logFile == NULL)
-  {
-    printf("WARNING: Unsuccessful in creating log file!");
-  }
+	// Sniffing in promiscuous mode
+	printf("\nOpening device '%s' for sniffing ... ", pDeviceName);
+	pHandle = pcap_open_live(pDeviceName, BUFSIZ, 1, 10000, errorBuffer);
+	if (pHandle == NULL)
+	{
+		fprintf(stderr, "Couldn't open device '%s': %s\n", pDeviceName, errorBuffer);
+		exit(EXIT_FAILURE);
+	}
+	printf("Done\n");
 
-  if(argc > 2){
-    // Compile and apply packet filter
-    if (pcap_compile(pHandle, &filter, argv[2], 0, mask) == -1)
-    {
-      fprintf(stderr, "Error compiling packet filter: %s\n", argv[2]);
-      exit(EXIT_FAILURE);
-    }
-    if (pcap_setfilter(pHandle, &filter) == -1)
-    {
-      fprintf(stderr, "Unable to set requested packet filter: %s\n", argv[2]);
-      exit(EXIT_FAILURE);
-    }
-  }
+	// Open a log file handler for traffic parsing output
+	logFile = fopen("log.txt", "w");
+	if (logFile == NULL)
+	{
+		printf("WARNING: Unsuccessful in creating log file!");
+	}
 
-  /* Put the selected device in a packet capture loop with callback function
-  'process_packet()' */
-  pcap_loop(pHandle, atoi(argv[1]), process_packet, callbackArgs);
-  fprintf(stdout, "Finished processing packets. Running clean-up ...\n");
+	if (argc > 2)
+	{
+		// Compile and apply packet filter
+		if (pcap_compile(pHandle, &filter, argv[2], 0, mask) == -1)
+		{
+			fprintf(stderr, "Error compiling packet filter: %s\n", argv[2]);
+			exit(EXIT_FAILURE);
+		}
+		if (pcap_setfilter(pHandle, &filter) == -1)
+		{
+			fprintf(stderr, "Unable to set requested packet filter: %s\n", argv[2]);
+			exit(EXIT_FAILURE);
+		}
+	}
 
-  // Free all the devices from memory
-  printf("Freeing all network devices and closing session ... ");
-  pcap_freealldevs(pAllDevices);
-  pcap_close(pHandle); // Close session
-  printf("Done\n");
-  exit(EXIT_SUCCESS);
+	/* Put the selected device in a packet capture loop with callback function
+	'process_packet()' */
+	pcap_loop(pHandle, atoi(argv[1]), process_packet, callbackArgs);
+
+	// Free all the devices from memory
+	printf("Freeing all network devices and closing session ... ");
+	pcap_freealldevs(pAllDevices);
+	pcap_close(pHandle); // Close session
+	printf("Done\n");
+
+	printf("\n\nICMP: %d  IGMP: %d  TCP: %d  UDP: %d  Other: %d  Total: %d\n",
+		   icmp_count, igmp_count, tcp_count, udp_count, other_count, total);
+	exit(EXIT_SUCCESS);
 }
 
 void process_packet(u_char *args, const struct pcap_pkthdr *header,
-                    const u_char *packet)
+					const u_char *packet)
 {
-  // Get the ethernet protocol type 
-  u_int16_t packetType = parse_ethernet_header(args, header, packet);
+	// Get the ethernet protocol type
+	u_int16_t packetType = parse_ethernet_header(args, header, packet);
 
-  if(packetType == ETHERTYPE_IP){
-    // Handle IP packet 
-  }
-  else if(packetType == ETHERTYPE_ARP){
-    // Handle ARP packet 
-  }
-  else if(packetType == ETHERTYPE_REVARP){
-    // Hadle reverse ARP packet
-  }
+	// Parse IP packet
+	if (packetType == ETHERTYPE_IP)
+	{
+		parse_ip_packet(args, header, packet);
+	}
+	else if (packetType == ETHERTYPE_ARP)
+	{
+		// Handle ARP packet
+	}
+	else if (packetType == ETHERTYPE_REVARP)
+	{
+		// Hadle reverse ARP packet
+	}
 }
 
 u_int16_t parse_ethernet_header(u_char *args, const struct pcap_pkthdr *header,
-                                const u_char *packet)
+								const u_char *packet)
 {
+	struct ether_header *pEthernetHeader; // Declared in 'net/ethernet.h'
+	pEthernetHeader = (struct ether_header *)packet;
 
-  static int count = 1;                    // Packet counter 
-  printf("\nPacket number %d:\n", count);
-  count++;
+	// Convert ethernet address to ASCII
+	fprintf(logFile, "\n");
+	fprintf(logFile, "Ethernet Header\n");
+	fprintf(logFile, "    | Destination MAC: %s \n", ether_ntoa((const struct ether_addr *)pEthernetHeader->ether_shost));
+	fprintf(logFile, "    | Source MAC     : %s \n", ether_ntoa((const struct ether_addr *)pEthernetHeader->ether_dhost));
 
-  struct ether_header *pEthernetHeader;    // Declared in 'net/ethernet.h'
+	// Print the ethernet protocol type
+	const uint16_t protocol = ntohs(pEthernetHeader->ether_type);
+	switch (protocol)
+	{
+	case ETHERTYPE_IP: // IPv4 type
+		fprintf(logFile, "    | Protocol       : IP \n");
+		break;
 
-  pEthernetHeader = (struct ether_header *) packet; 
-  
-  // Convert ethernet address to ASCII 
-  fprintf(stdout, "  ethernet header: source-> %s  ", ether_ntoa((const struct ether_addr *) pEthernetHeader->ether_shost));
-  fprintf(stdout, "destination -> %s", ether_ntoa((const struct ether_addr *)pEthernetHeader->ether_dhost));
+	case ETHERTYPE_ARP:
+		fprintf(logFile, "    | Protocol       : ARP \n");
+		break;
 
-  // Print the ethernet protocol type
-  switch (ntohs(pEthernetHeader->ether_type))
-  {
-    case ETHERTYPE_IP: // IPv4 type
-      fprintf(stdout, " (IP)");
-      break;
+	case ETHERTYPE_REVARP:
+		fprintf(logFile, "    | Protocol       : RARP \n");
+		break;
 
-    case ETHERTYPE_ARP:
-      fprintf(stdout, " (ARP)");
-      break;
+	default: // Some other ethernet protocol. See 'net/ethernet.h'
+		fprintf(logFile, "    | Protocol       : ? (%d) \n", protocol);
+		break;
+	}
 
-    case ETHERTYPE_REVARP:
-      fprintf(stdout, " (RARP)");
-      break;
+	return ntohs(pEthernetHeader->ether_type);
+}
 
-    default: // Some other ethernet protocol. See 'net/ethernet.h'
-      fprintf(stdout, " (?)");
-      break;
-  }
-  fprintf(stdout, "\n");
+void parse_ip_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
+{
+	struct iphdr *pIPHeader = (struct iphdr *)(packet + sizeof(struct ether_header));
+	++total;
 
-  return pEthernetHeader->ether_type; 
+	switch (pIPHeader->protocol) // See RFC 790 for protocol-value mapping
+	{
+	case 1: // ICMP protocol
+		++icmp_count;
+		printf("Received ICMP packet!\n");
+		break;
+
+	case 2: // IGMP protocol
+		++igmp_count;
+		printf("Received IGMP packet!\n");
+		break;
+
+	case 6: // TCP protocol
+		++tcp_count;
+		printf("Received TCP packet!\n");
+		break;
+
+	case 17: // UDP protocol
+		++udp_count;
+		printf("Received UDP packet!\n");
+		break;
+
+	default: // Another protocol like Telnet
+		++other_count;
+		printf("Received other packet!\n");
+		break;
+	}
 }
