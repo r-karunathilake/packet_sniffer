57c667e0d86d862d4e1a73eccedd744caa417065
Ravindu Karunathilake <karunath@ualberta.ca>
2023-11-03 13:49:37 -0600

    Added parsed IP header logging functionality.

--- packet_sniffer.c
+++ packet_sniffer.c
@@ -13,7 +13,7 @@
 */
 
 #include <stdio.h>
-#include <string.h>		   // For strcpy()
+#include <string.h>		   // For strcpy() and memset() 
 #include <pcap.h>		   // Access copies of packets off the wire.
 #include <stdlib.h>		   // For exit()
 #include <netinet/in.h>	   // 'in_addr' structure declaration used by 'inet_ntoa()'
@@ -23,18 +23,22 @@
 #include <netinet/ip.h>	   // Provides IP header declaration
 
 FILE *logFile = NULL;
-// Count the number of IP packets
+// Count the number of packets
 static int total = 0;
 static int icmp_count = 0;
 static int igmp_count = 0;
 static int tcp_count = 0;
 static int udp_count = 0;
 static int other_count = 0;
+static int invalid_count = 0;
 
 // Function prototypes
 void process_packet(u_char *, const struct pcap_pkthdr *, const u_char *);
-u_int8_t parse_ip_packet(u_char *, const struct pcap_pkthdr *, const u_char *);
-u_int16_t parse_ethernet_header(u_char *, const struct pcap_pkthdr *, const u_char *);
+void log_ethernet_header(u_char *, const u_char *);
+void log_ip_header(u_char *, const u_char *);
+void log_icmp_packet(u_char *, const struct pcap_pkthdr *, const u_char *);
+int get_ip_protocol(u_char *, const struct pcap_pkthdr *, const u_char *);
+u_int16_t get_eth_protocol(u_char *, const u_char *);
 
 int main(int argc, char *argv[])
 {
@@ -43,8 +47,8 @@ int main(int argc, char *argv[])
 	pcap_if_t *pDevice = NULL;			// A network interface
 	char *pDeviceName = NULL;			// The network interface name to be sniffed
 	char errorBuffer[PCAP_ERRBUF_SIZE]; // Error string of size 256
-	char *ip_addr = NULL;				// Dot notation IP address
-	char *mask_addr = NULL;				// Dot notation mask address
+	const char *ip_addr = NULL;		    // Dot notation IP address
+	const char *mask_addr = NULL;		// Dot notation mask address
 	bpf_u_int32 mask;					// Interface network mask
 	bpf_u_int32 ip;						// Interface IP
 	struct bpf_program filter;			// Compiled filter structure
@@ -92,19 +96,21 @@ int main(int argc, char *argv[])
 	// Convert the IP and mask from bytes to IPv4 dotted-decimal notation
 	struct in_addr addr; // IPv4 address conversion structure
 	addr.s_addr = ip;
-	ip_addr = inet_ntoa(addr);
+
+	char writeBuffer[INET_ADDRSTRLEN];
+	ip_addr = inet_ntop(AF_INET, &addr, writeBuffer, INET_ADDRSTRLEN);
 	if (ip_addr == NULL)
 	{
-		perror("inet_ntoa()");
+		perror("inet_ntop()");
 		exit(EXIT_FAILURE);
 	}
 	printf("  IP: %s\n", ip_addr);
 
 	addr.s_addr = mask;
-	mask_addr = inet_ntoa(addr);
+	mask_addr = inet_ntop(AF_INET, &addr, writeBuffer, INET_ADDRSTRLEN);
 	if (mask_addr == NULL)
 	{
-		perror("inet_ntoa()");
+		perror("inet_ntop()");
 		exit(EXIT_FAILURE);
 	}
 	printf("  Mask: %s\n", mask_addr);
@@ -160,38 +166,43 @@ void process_packet(u_char *args, const struct pcap_pkthdr *header,
 					const u_char *packet)
 {
 	// Get the ethernet protocol type
-	u_int16_t packetType = parse_ethernet_header(args, header, packet);
+	u_int16_t packetType = get_eth_protocol(args, packet);
 
 	// Parse IP packet
 	if (packetType == ETHERTYPE_IP)
 	{
-		uint8_t ipProtocol = parse_ip_packet(args, header, packet);
+		int ipProtocol = get_ip_protocol(args, header, packet);
 		switch (ipProtocol) // See RFC 790 for protocol-value mapping
 		{
-			case 1: // ICMP protocol
-				++icmp_count;
-				fprintf(logFile, "*****Received ICMP packet!*****\n");
-				break;
+		case 1: // ICMP protocol
+			++icmp_count;
+			log_icmp_packet(args, header, packet);
+			break;
 
-			case 2: // IGMP protocol
-				++igmp_count;
-				printf("Received IGMP packet!\n");
-				break;
+		case 2: // IGMP protocol
+			++igmp_count;
+			printf("Received IGMP packet!\n");
+			break;
 
-			case 6: // TCP protocol
-				++tcp_count;
-				printf("Received TCP packet!\n");
-				break;
+		case 6: // TCP protocol
+			++tcp_count;
+			printf("Received TCP packet!\n");
+			break;
 
-			case 17: // UDP protocol
-				++udp_count;
-				printf("Received UDP packet!\n");
-				break;
+		case 17: // UDP protocol
+			++udp_count;
+			printf("Received UDP packet!\n");
+			break;
 
-			default: // Another protocol like Telnet and invalid protocols 
-				++other_count;
-				printf("Received other packet!\n");
-				break;
+		case -1: // Invalid packet
+			++invalid_count;
+			printf("Received Invalid packet! Dumping raw data to log");
+			break;
+
+		default: // Another protocol like Telnet
+			++other_count;
+			printf("Received other packet!\n");
+			break;
 		}
 	}
 	else if (packetType == ETHERTYPE_ARP)
@@ -200,12 +211,50 @@ void process_packet(u_char *args, const struct pcap_pkthdr *header,
 	}
 	else if (packetType == ETHERTYPE_REVARP)
 	{
-		// Hadle reverse ARP packet
+		// Handle reverse ARP packet
 	}
 }
 
-u_int16_t parse_ethernet_header(u_char *args, const struct pcap_pkthdr *header,
-								const u_char *packet)
+uint16_t get_eth_protocol(u_char *args, const u_char *packet)
+{
+	struct ether_header *pEthernetHeader; // Declared in 'net/ethernet.h'
+	pEthernetHeader = (struct ether_header *)packet;
+
+	return ntohs(pEthernetHeader->ether_type);
+}
+
+int get_ip_protocol(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
+{
+	struct iphdr *pIPHeader = (struct iphdr *)(packet + sizeof(struct ether_header));
+	u_int size = header->len;
+	if (size < sizeof(struct iphdr))
+	{
+		fprintf(stderr, "Warning: * Truncated IP packet detected (expected %lu bytes got %d bytes)", sizeof(struct iphdr), size);
+		return -1; // Invalid protocol
+	}
+
+	// IP header has no fixed length; its length is given in 4-byte words
+	u_int ipHeaderLength = pIPHeader->ihl * 4;
+	if (ipHeaderLength < 20)
+	{
+		printf("Warning: * Invalid IP header length: %u bytes (expected minimum 20 bytes)\n", ipHeaderLength);
+		return -1;
+	}
+
+	++total; // Valid IP packet detected
+	return (int)pIPHeader->protocol;
+}
+
+void log_icmp_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
+{
+	fprintf(logFile, "\n===================================ICMP Packet===================================");
+	log_ethernet_header(args, packet);
+	log_ip_header(args, packet);
+
+	// fprintf(logFile, "\n*********************************************************************************");
+}
+
+void log_ethernet_header(u_char *args, const u_char *packet)
 {
 	struct ether_header *pEthernetHeader; // Declared in 'net/ethernet.h'
 	pEthernetHeader = (struct ether_header *)packet;
@@ -236,23 +285,38 @@ u_int16_t parse_ethernet_header(u_char *args, const struct pcap_pkthdr *header,
 		fprintf(logFile, "    | Protocol       : ? (%d) \n", protocol);
 		break;
 	}
-
-	return ntohs(pEthernetHeader->ether_type);
 }
 
-uint8_t parse_ip_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
+void log_ip_header(u_char *args, const u_char *packet)
 {
+	// Parse and log IPv4 header 
 	struct iphdr *pIPHeader = (struct iphdr *)(packet + sizeof(struct ether_header));
-	u_int size = header->len;
-	if (size < sizeof(struct iphdr))
-	{
-		fprintf(stderr, "Warning: Truncated IP packet header detected (expected %lu bytes got %d bytes)", sizeof(struct iphdr), size);
-		return -1; // Invalid protocol 
-	}
-	++total; // Valid IP packet detected
 
-	//TODO: call the ethernet header parser from inside the ip parser 
-	//TODO: log the parsed IP packet
+	// See RFC 791
+	fprintf(logFile, "\n");
+	fprintf(logFile, "IP Header\n");
+	fprintf(logFile, "    | IP Version             : %u \n", pIPHeader->version);
+	fprintf(logFile, "    | Header Length          : %u Bytes \n", pIPHeader->ihl * 4);
+	fprintf(logFile, "    | Type of Service        : %u \n", pIPHeader->tos);
+	fprintf(logFile, "    | Total Length           : %u Bytes \n", ntohs(pIPHeader->tot_len));
+	fprintf(logFile, "    | Identification         : %u \n", ntohs(pIPHeader->id));
+	fprintf(logFile, "    | Fragment Offset        : %u Bytes \n", ntohs(pIPHeader->frag_off));
+	fprintf(logFile, "    | Time to Live           : %u \n", pIPHeader->ttl);
+	fprintf(logFile, "    | Transport Protocol     : %u \n", pIPHeader->protocol);
+	fprintf(logFile, "    | Checksum               : %u \n", ntohs(pIPHeader->check));
 
-	return pIPHeader->protocol;
+	// Parse the source and destination IP addresses
+	char writeBuffer[INET_ADDRSTRLEN];
+	struct in_addr sourceAddr;
+	struct in_addr destAddr;
+
+	sourceAddr.s_addr = pIPHeader->saddr;
+	destAddr.s_addr = pIPHeader->daddr;
+
+	fprintf(logFile, "    | Source IP Address      : %s \n", inet_ntop(AF_INET, &sourceAddr, 
+	                                                                   writeBuffer, INET_ADDRSTRLEN));
+	fprintf(logFile, "    | Destination IP Address : %s \n", inet_ntop(AF_INET, &destAddr, 
+	                                                                   writeBuffer, INET_ADDRSTRLEN));
 }
+
+
